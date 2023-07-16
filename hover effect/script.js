d6d8b58a9a6e4de806b142947b4732bbb8f51b87
maunak <maunakdass@gmail.com>
2023-07-16 20:22:24 +0530

    changes 6

---
+++ hover effect/script.js
@@ -0,0 +1,14 @@
+const blocks = document.querySelectorAll(".block");
+const resetDuration = 300;
+
+blocks.forEach((block) => {
+    let timeoutId;
+
+    block.addEventListener("mouseover", () => {
+        clearTimeout(timeoutId);
+        block.classList.add("active");
+        timeoutId = setTimeout(() => {
+            block.classList.remove("active");
+        }, resetDuration);
+    });
+});
